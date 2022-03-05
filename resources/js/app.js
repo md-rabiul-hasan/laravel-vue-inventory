534bc14e5fafed2d2f20a053a588f17af436f308
rabiulhasan3 <rabiul.fci@gmail.com>
2022-03-06 02:07:50 +0600

    user helper

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,10 @@ import {routes} from './routes'
 
 Vue.use(VueRouter)
 
+// global import 
+import User from './Helpers/User'
+window.User = User;
+
 const router = new VueRouter({
     mode: 'history',
     routes: routes
